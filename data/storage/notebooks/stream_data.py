6310f120a52eb0744231a78246a4f585dcc276f5
alexpeters1208 <alexscottpeters1208@gmail.com>
2024-08-20 18:20:34 -0500

    Refactor scripts

--- data/storage/notebooks/stream_data.py
+++ data/storage/notebooks/stream_data.py
@@ -1,9 +1,9 @@
 #### This script reads all of the Amazon data from Parquet into Deephaven tables
-#### Then, it simulates a table receiving the reviews at 100,000x real-time speed.
+#### Then, it simulates a table receiving the reviews at 10,000x real-time speed.
 
 from deephaven import parquet
 from deephaven import dtypes
-from deephaven.column import Column
+from deephaven.table import TableDefinition
 from deephaven.replay import TableReplayer
 from deephaven.time import to_j_instant
 
@@ -12,14 +12,14 @@ from math import floor
 from jpy import array
 
 # create table definition for review datasets
-reviews_def = [
-    Column("rating", dtypes.double),
-    Column("title", dtypes.string),
-    Column("text", dtypes.string),
-    Column("parent_asin", dtypes.string),
-    Column("user_id", dtypes.string),
-    Column("timestamp", dtypes.long)
-]
+reviews_def = TableDefinition({
+    "rating": dtypes.double,
+    "title": dtypes.string,
+    "text": dtypes.string,
+    "parent_asin": dtypes.string,
+    "user_id": dtypes.string,
+    "timestamp": dtypes.long
+})
 
 # read reviews into a single table
 reviews = parquet.read(
@@ -28,48 +28,31 @@ reviews = parquet.read(
     table_definition=reviews_def
 )
 
-# create table definition for meta datasets
-items_def = [
-    Column("main_category", dtypes.string),
-    Column("title", dtypes.string),
-    Column("average_rating", dtypes.float64),
-    Column("rating_number", dtypes.int64),
-    Column("parent_asin", dtypes.string)
-]
-
-# read item metadata into a single table
-items = parquet.read(
-    "/data/amazon/meta/",
-    file_layout=parquet.ParquetFileLayout.FLAT_PARTITIONED,
-    table_definition=items_def
-)
-
-# update reviews, filter for post-2020
+# convert timestamp to Java timestamps for replay
 reviews = (
     reviews
     .update("timestamp = epochMillisToInstant(timestamp)")
-    .where("year(timestamp, 'UTC') >= 2020")
     .sort("timestamp")
     .update("idx = ii")
 )
 
 # minimum and maximum times from filtered table - faster to use UI than to compute with a query
-min_time = to_j_instant("2020-01-01T00:00:00.000Z")
+min_time = to_j_instant("2023-01-01T00:00:00.000Z")
 max_time = to_j_instant("2023-09-14T13:16:54.993Z")
 
-# create replay start time
+# create replay start time and end time
 replay_start_time = to_j_instant("2024-01-01T00:00:00Z")
-replay_end_time = to_j_instant("2024-01-01T00:30:00Z")
+replay_end_time = to_j_instant("2024-01-01T00:36:00Z")
 
 # this parameter controls the number of seconds per second that data will tick
 # also controls the sparsity of the data, so that number of rows per second will be approx. real time
-data_speed = 100_000
+data_speed = 10_000
 
 # randomly sample data to get approx. real time density
 random_idx = sample(range(reviews.size), k=floor(reviews.size / data_speed))
 j_random_idx = array("long", random_idx)
 
-# create a replay timestamp
+# randomly sample data and create a replay timestamp
 reviews = (
     reviews
     .where("idx in j_random_idx")
--- data/storage/notebooks/to_parquet.py
+++ data/storage/notebooks/to_parquet.py
@@ -1,13 +1,12 @@
-#### This script downloads the Amazon data using Huggingface datasets and writes them to Parquet files.
-
 import os
 import datasets
 import pyarrow.parquet as pq
+import datetime as dt
 datasets.logging.set_verbosity_error()
 
 from datasets import load_dataset
 
-# Names of item metadata datasets
+# Names of all dataset categories
 names = [
     'All_Beauty',
     'Toys_and_Games',
@@ -45,14 +44,28 @@ names = [
     'Movies_and_TV'
 ]
 
+# only want to look at data in 2023 - get Jan 1 2023 and convert to millis since epoch
+start_time_millis = dt.datetime(2023, 1, 1, 0, 0, 0, tzinfo=dt.timezone.utc).timestamp() * 1_000
+
 for name in names:
 
     # load reviews and write as Parquet if dataset does not already exist
-    if not os.path.exists(f"data/amazon/review/{name}.parquet"):
+    if not os.path.exists(f"data/amazon/review1/{name}.parquet"):
         review_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", f"raw_review_{name}", trust_remote_code=True)['full']
-        pq.write_table(review_dataset.data.table, f"data/amazon/review/{name}.parquet")
+        # select columns of interest and filter for post-2022 before writing
+        filtered_review_dataset = (
+            review_dataset
+            .select_columns(["rating", "title", "text", "parent_asin", "user_id", "timestamp"])
+            .filter(lambda timestamp: timestamp >= start_time_millis, input_columns="timestamp")
+        )
+        filtered_review_dataset.to_parquet(f"data/amazon/review1/{name}.parquet")
 
     # load metadata and write as Parquet if dataset does not already exist
-    if not os.path.exists(f"data/amazon/meta/{name}.parquet"):
+    if not os.path.exists(f"data/amazon/meta1/{name}.parquet"):
         meta_dataset = load_dataset("McAuley-Lab/Amazon-Reviews-2023", f"raw_meta_{name}", trust_remote_code=True)['full']
-        pq.write_table(meta_dataset.data.table, f"data/amazon/meta/{name}.parquet")
+        # select columns of interest before writing
+        filtered_meta_dataset = (
+            meta_dataset
+            .select_columns(["main_category", "title", "average_rating", "rating_number", "parent_asin"])
+        )
+        filtered_meta_dataset.to_parquet(f"data/amazon/meta1/{name}.parquet")
